45bdff4c3ee9c5da2fd2cde65c95ccbed6a06c36
scooby75 <45230721+scooby75@users.noreply.github.com>
2025-05-27 12:46:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ avaliacao_mercados = {
     "Odd Calculada": [f"{odd_calculada_casa:.2f}", f"{odd_calculada_empate:.2f}", f"{odd_calculada_fora:.2f}", f"{odd_calculada_dnb:.2f}", f"{odd_calculada_1x:.2f}", f"{odd_calculada_x2:.2f}"],
     "Odd Informada": [f"{odd_casa:.2f}", f"{odd_empate:.2f}", f"{odd_visitante:.2f}", f"{odd_dnb_casa:.2f}", f"{odd_1x:.2f}", f"{odd_x2:.2f}"],
     "Aposta +EV": [
-        f"{avaliar_ev(odd_casa, odd_calculada_casa)} ({calcular_percentual_ev(odd_casa, odd_calculada_casa):.2f}%)",
+        f"({calcular_percentual_ev(odd_casa, odd_calculada_casa):.2f}%)",
         f"{avaliar_ev(odd_empate, odd_calculada_empate)} ({calcular_percentual_ev(odd_empate, odd_calculada_empate):.2f}%)",
         f"{avaliar_ev(odd_visitante, odd_calculada_fora)} ({calcular_percentual_ev(odd_visitante, odd_calculada_fora):.2f}%)",
         f"{avaliar_ev_dnb(odd_dnb_casa, prob_total_casa, prob_total_empate, prob_total_fora)} ({calcular_percentual_ev(odd_dnb_casa, odd_calculada_dnb):.2f}%)",
